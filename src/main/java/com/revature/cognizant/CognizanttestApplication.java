02014b1d2942ce12e111d24feb519f9ed33ae41c
George Sayapin <prorabserv@gmail.com>
2020-02-21 10:04:10 -0500

    Project template creation

---
+++ src/main/java/com/revature/cognizant/CognizanttestApplication.java
@@ -0,0 +1,13 @@
+package com.revature.cognizant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CognizanttestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CognizanttestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/revature/cognizant/CognizanttestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.revature.cognizant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CognizanttestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
